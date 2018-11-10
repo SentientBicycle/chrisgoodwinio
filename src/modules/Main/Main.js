1a37856a24b291250e08ad3417b05ebd6df566ac
Chris Goodwin <chrisj.goodwin@gmail.com>
2018-11-10 10:15:21 -0800

    Added image and set figure in main.

--- src/modules/Main/Main.js
+++ src/modules/Main/Main.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import style from './Main.module.css';
 import DropFades from './components/DropFade/DropFades';
+import HeaderImage from './resources/my-life-through-a-lens-110632-unsplash.jpg'
 
 class Main extends Component {
   render() {
     return (
+    	<React.Fragment>
       <section className={style.main}>
         <h1 className={style['title-header']}>Chris Goodwin</h1>
         <DropFades/>
-      	<p>Hi, I'm a frontend developer.  I made everything you see before you.  From scratch, or at least a baseline cli.</p>      	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis urna cursus eget nunc. Neque gravida in fermentum et sollicitudin ac orci. Id donec ultrices tincidunt arcu. In egestas erat imperdiet sed euismod. Ac turpis egestas maecenas pharetra convallis posuere. Id diam maecenas ultricies mi. Velit aliquet sagittis id consectetur purus ut faucibus. Massa tempor nec feugiat nisl pretium fusce id velit. Libero nunc consequat interdum varius. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Tincidunt augue interdum velit euismod in. Sit amet commodo nulla facilisi nullam. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
-      	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis urna cursus eget nunc. Neque gravida in fermentum et sollicitudin ac orci. Id donec ultrices tincidunt arcu. In egestas erat imperdiet sed euismod. Ac turpis egestas maecenas pharetra convallis posuere. Id diam maecenas ultricies mi. Velit aliquet sagittis id consectetur purus ut faucibus. Massa tempor nec feugiat nisl pretium fusce id velit. Libero nunc consequat interdum varius. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Tincidunt augue interdum velit euismod in. Sit amet commodo nulla facilisi nullam. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
-      	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis urna cursus eget nunc. Neque gravida in fermentum et sollicitudin ac orci. Id donec ultrices tincidunt arcu. In egestas erat imperdiet sed euismod. Ac turpis egestas maecenas pharetra convallis posuere. Id diam maecenas ultricies mi. Velit aliquet sagittis id consectetur purus ut faucibus. Massa tempor nec feugiat nisl pretium fusce id velit. Libero nunc consequat interdum varius. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Tincidunt augue interdum velit euismod in. Sit amet commodo nulla facilisi nullam. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
-      	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis urna cursus eget nunc. Neque gravida in fermentum et sollicitudin ac orci. Id donec ultrices tincidunt arcu. In egestas erat imperdiet sed euismod. Ac turpis egestas maecenas pharetra convallis posuere. Id diam maecenas ultricies mi. Velit aliquet sagittis id consectetur purus ut faucibus. Massa tempor nec feugiat nisl pretium fusce id velit. Libero nunc consequat interdum varius. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Tincidunt augue interdum velit euismod in. Sit amet commodo nulla facilisi nullam. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
+      	<p>Hello and welcome to my humble website.  I made everything you see before you from scratch.</p>
       </section>
+      <section>
+      <figure className={style.figure}>
+      		<img className={style.image} src={HeaderImage} />
+      	</figure>
+      </section>
+      </React.Fragment>
     );
   }
 }
--- src/modules/Main/components/DropFade/DropFades.js
+++ src/modules/Main/components/DropFade/DropFades.js
@@ -8,7 +8,7 @@ class DropFades extends Component {
 
 
   render (){
-  	const words = ['Is\u00a0Hard\u00a0Working', 'Is\u00a0Efficient', 'Is\u00a0Focused', 'Is\u00a0Energetic', 'Has\u00a0a\u00a0Pteradactyl', 'Is\u00a0Color\u00a0Blind', 'Can\'t\u00a0Digest\u00a0Lactose' ];
+  	const words = ['Is\u00a0Hard\u00a0Working', 'Is\u00a0Efficient', 'Is\u00a0Focused', 'Is\u00a0Energetic', 'Has\u00a0a\u00a0Pteradactyl'];
   	const tagtype = 'h2';
   	return (
   		<div className={styles['parent-container']}>
