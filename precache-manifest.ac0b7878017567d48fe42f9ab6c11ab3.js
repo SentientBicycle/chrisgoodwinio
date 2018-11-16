c1b2e87f1413570aed03b93b361f8f4c66a04b5a
Chris Goodwin <chrisj.goodwin@gmail.com>
2018-11-16 14:03:11 -0800

    Updates

---
+++ precache-manifest.ac0b7878017567d48fe42f9ab6c11ab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "393908da9784fc1ecfde",
+    "url": "/static/css/main.b4ec5d1e.chunk.css"
+  },
+  {
+    "revision": "393908da9784fc1ecfde",
+    "url": "/static/js/main.393908da.chunk.js"
+  },
+  {
+    "revision": "851023fa7fbb271e76be",
+    "url": "/static/css/1.e163b6dc.chunk.css"
+  },
+  {
+    "revision": "851023fa7fbb271e76be",
+    "url": "/static/js/1.851023fa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bcbb4554f4c72460dfc375ce9780b54b",
+    "url": "/static/media/my-life-through-a-lens-110632-unsplash.bcbb4554.jpg"
+  },
+  {
+    "revision": "7f6d23e9a423014a14a5bacb2a9fe567",
+    "url": "/index.html"
+  }
+];
