2649c1c9e3a77a5684bc93e0376fe4a8dc86e319
Chris Goodwin <chrisj.goodwin@gmail.com>
2018-11-16 10:56:24 -0800

    evidently using onpropsrecieved is an anti-pattern.  Removed extraneous code.

--- src/modules/Main/components/DropFade/DropFade.js
+++ src/modules/Main/components/DropFade/DropFade.js
@@ -2,18 +2,9 @@ import React, { Component } from 'react';
 import styles from './DropFade.module.css';
 
 class DropFade extends Component {
-	state = {
-		active: this.props.active,
-		place: 0,
-	}
 
 	componentDidMount() {
 		this.df.addEventListener('animationend', this.props.setNextActive);
-		this.setState(() => {if(this.props.place === 0)return { active: true}})
-	}
-
-	componentWillReceiveProps(newProps) {
-		this.setState({active: newProps.active});
 	}
 	
 
@@ -21,13 +12,13 @@ class DropFade extends Component {
   	const CustomTag = `${this.props.tagtype}`;
   	return (
   		/*  
-		  *  Outputs a paragraph tag with the initial element containing the active class.
+		  *  Outputs a custom tag with the initial element containing the active class.
 		  *  place: the index of the created element.  Not used right now, however may be
 		  *         useful when adding redux.
 		  *  word: the actual content of the tag.
 		  *  reg:  This is a reference callback to add the animationend event listener.
   		  */
-	  	<CustomTag  ref={elem=> this.df = elem} className={`${styles.fader} ${this.state.active === true ? styles.active : ''}`} place={this.props.place} >{`${this.props.word}`}</CustomTag>
+	  	<CustomTag  ref={elem=> this.df = elem} className={`${styles.fader} ${this.props.active === true ? styles.active : ''}`} place={this.props.place} >{`${this.props.word}`}</CustomTag>
   	
 	);
   }
